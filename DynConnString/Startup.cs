28bae0546a6c7144fb4c36b877f144ad83401e17
JT <coreprogramm@gmail.com>
2020-04-12 13:53:54 +0530

    Global Delcare Connection String

---
+++ DynConnString/Startup.cs
@@ -0,0 +1,67 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using DynConnString.Models;
+
+namespace DynConnString
+{
+    public class Startup
+    {
+        public IConfigurationRoot ConfigurationRoot { get; set; }
+        public static string ConnectionString { get; private set; }
+      
+        public Startup(IConfiguration config,IWebHostEnvironment env)
+        {
+
+            ConfigurationRoot = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appSettings.json")
+                .Build();
+        }
+        public IConfiguration Configuration { get; set; }
+
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+           
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+           
+            ConnectionString = ConfigurationRoot.GetConnectionString("DefaultConnection");
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+           
+        }
+    
+    }
+}
